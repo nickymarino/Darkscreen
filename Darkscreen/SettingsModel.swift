5999cd06615df390a378c806a624a57b5ceea90f
Nicky Marino <nsmarino96@gmail.com>
2019-02-15 18:39:23 -0500

    Just have the screen be black

---
+++ Darkscreen/SettingsModel.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsModel.swift
+//  Darkscreen
+//
+//  Created by Nicky Marino on 2/15/19.
+//  Copyright © 2019 Nicky Marino. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsModel {
+    let BACKGROUND_COLOR = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0);
+}
